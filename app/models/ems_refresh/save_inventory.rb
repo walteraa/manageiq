fc90f8c850f23742930883245ce29a877245073e
Tercio de Melo <terciodemelo@gmail.com>
2016-11-18 10:32:50 -0300

    Changes matching order of managers in save_inventory

--- app/models/ems_refresh/save_inventory.rb
+++ app/models/ems_refresh/save_inventory.rb
@@ -21,7 +21,7 @@ module EmsRefresh::SaveInventory
     when ManageIQ::Providers::DatawarehouseManager          then save_ems_datawarehouse_inventory(ems, hashes, target)
     end
   end
-
+ 
   #
   # Shared between Cloud and Infra
   #
