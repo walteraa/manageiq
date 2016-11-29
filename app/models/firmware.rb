9427a71ad695915f788c485e1e52866047b877b5
Walter Alves <walter.arruda.alves@gmail.com>
2016-11-29 17:30:23 -0300

    Firmware migrate
    Database migration to persist Firmware data.
    A firmware is associated to a Physical Server.

---
+++ app/models/firmware.rb
@@ -0,0 +1,13 @@
+class Firmware < ApplicationRecord
+  include NewWithTypeStiMixin
+
+  acts_as_miq_taggable
+
+
+  belongs_to :physical_server, :foreign_key => :ph_server_id, :class_name => "PhysicalServer"
+
+
+
+
+
+end
