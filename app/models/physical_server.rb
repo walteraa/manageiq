6c941d81a586b9b0abde3f728c1dd52153b799b9
Walter Alves <walter.arruda.alves@gmail.com>
2016-12-14 18:11:19 -0300

    Change the relationship between firmware and physical server
    Changing has_many params into the physical server model.

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -1,15 +1,11 @@
 class PhysicalServer < ApplicationRecord
   include NewWithTypeStiMixin
 
+
   acts_as_miq_taggable
 
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
-
-  def firmwares
-    Firmware.where("ph_server_uuid = ?",  uuid)
-
-  end
-
+  has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
 
   def name_with_details
     details % {
