abccdd92999383d80381b6909438c00707e676f6
Tercio de Melo <terciodemelo@gmail.com>
2016-12-15 09:48:18 -0300

    [PhysicalServers] Associates PhysicalServers to Hosts

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -1,11 +1,11 @@
 class PhysicalServer < ApplicationRecord
   include NewWithTypeStiMixin
 
-
   acts_as_miq_taggable
 
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
   has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
+  has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
 
   def name_with_details
     details % {
