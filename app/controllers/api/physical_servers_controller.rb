3b072337cd3c7cd4084d49dd05bb6c5d53080fc0
Tercio de Melo <terciodemelo@gmail.com>
2016-12-16 10:51:18 -0300

    [PhysicalServers] Includes host_id in physical servers API

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -8,12 +8,15 @@ module Api
         response_payload = physical_server.as_json
         firmwares = physical_server.firmwares.map(&:as_json)
         response_payload["firmwares"] = firmwares
+        response_payload["host_id"] = case physical_server.host
+                                      when nil then nil
+                                      else physical_server.host.id
+                                      end
 
         render json: response_payload
       else
         super
       end
     end
-  
   end
 end
