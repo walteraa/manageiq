8a73f5b900247e415c677b90accc4b304c0c0aba
Walter Alves <walter.arruda.alves@gmail.com>
2016-12-16 10:34:34 -0300

    Show physical server's firmwares in REST API
    All Firmwares is now showing into a physical server instance.

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -2,5 +2,18 @@ module Api
   class PhysicalServersController < BaseController
     include Subcollections::Firmwares
   
+    def show
+      if params[:c_id]
+        physical_server = PhysicalServer.find(params[:c_id])
+        response_payload = physical_server.as_json
+        firmwares = physical_server.firmwares.map(&:as_json)
+        response_payload["firmwares"] = firmwares
+
+        render json: response_payload
+      else
+        super
+      end
+    end
+  
   end
 end
