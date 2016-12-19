1611529dbb1858029e1cfa126af5caa555796f89
skovic <skovic@lenovo.com>
2016-12-19 11:40:39 -0500

    Initial support added for turning on/off loc leds

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -17,6 +17,19 @@ module Api
       else
         super
       end
+    
+    def turn_on_loc_led_resource(server)
+      task_id = queue_object_action(server, desc, :method_name => "turn_on_loc_led", :role => "ems_operations")
+      action_result(true, desc, :task_id => task_id)
+      rescue => err
+        action_result(false, err.to_s)
+    end
+
+    def turn_off_loc_led_resource(server)
+      task_id = queue_object_action(server, desc, :method_name => "turn_off_loc_led", :role => "ems_operations")
+      action_result(true, desc, :task_id => task_id)
+      rescue => err
+        action_result(false, err.to_s)
     end
   end
 end
