b0d33815d112dcf25dd5f264cff1da8a1b4aa707
Rodney H. Brown <rbrown4@lenovo.com>
2017-01-11 17:27:00 -0500

    Updates for rubocop results

--- app/models/physical_server/operations.rb
+++ app/models/physical_server/operations.rb
@@ -1,12 +1,7 @@
 module PhysicalServer::Operations
   extend ActiveSupport::Concern
 
-<<<<<<< 96bd625b773a324e62712f0adfce0bff7ec7d662
   include_concern 'Power'
-#include_concern 'Led'
-=======
-  #include_concern 'Power'
   include_concern 'Led'
->>>>>>> Add support for turning on/off LEDs - working ver
 
 end
