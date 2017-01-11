826af6e7c8166109f4ebe53981b963b57ab24098
Rodney H. Brown <rbrown4@lenovo.com>
2017-01-11 10:11:19 -0500

    Adds power action to POST capabilities
    POST to /physical_server/id with the JSON
    { 'action', 'power_on | power_off | restart'}
    
    Methods added to physical_servers_controller and
    the PhysicalServer model via a PhysicalServer::Operations concern

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -1,13 +1,9 @@
 class PhysicalServer < ApplicationRecord
   include NewWithTypeStiMixin
 
-  #include_concern 'Operations'
+  include_concern 'Operations'
 
-  acts_as_miq_taggable
-
-  belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
-  has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
-  has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
+  acts_as_miq_taggable  
 
   def name_with_details
     details % {
@@ -16,14 +12,13 @@ class PhysicalServer < ApplicationRecord
   end
 
   def my_zone
-   
     ems = ext_management_system
     ems ? ems.my_zone : MiqServer.my_zone
   end
 
 
   def turn_on_loc_led
-   $lenovo_log.info("Management System Name: #{ext_management_system.name}")
+   
    unless ext_management_system
       raise _(" A Server #{self} <%{name}> with Id: <%{id}> is not associated with a provider.") % {:name => name, :id => id}
     end
@@ -39,4 +34,7 @@ class PhysicalServer < ApplicationRecord
   def turn_off_loc_led
     $lenovo_log.info("Turn off LED")
   end
+
+ 
+
 end
