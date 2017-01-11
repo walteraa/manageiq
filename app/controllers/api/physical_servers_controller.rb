9b1c97c069bc23ce5545160b605f4f4b46b4b85c
Rodney H. Brown <rbrown4@lenovo.com>
2017-01-11 10:11:19 -0500

    Adds power action to POST capabilities
    POST to /physical_server/id with the JSON
    { 'action', 'power_on | power_off | restart'}
    
    Methods added to physical_servers_controller and
    the PhysicalServer model via a PhysicalServer::Operations concern

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -35,18 +35,39 @@ module Api
     end
 
       
+    #
+    # Name: power_on_resource
+    # Description: Power on server
+    #
+    def power_on_resource(type, id, _data)
+       change_resource_state(:power_on, type, id, _data)
+    end
 
-    def turn_off_loc_led_resource(server)
-      task_id = queue_object_action(server, desc, :method_name => "turn_off_loc_led", :role => "ems_operations")
-      action_result(true, desc, :task_id => task_id)
-      rescue => err
-        action_result(false, err.to_s)
+    def power_off_resource(type, id, _data)
+      change_resource_state(:power_off, type, id, _data)
+    end
+
+    def restart_resource(type, id, _data)
+      change_resource_state(:restart, type, id, _data)
     end
 
    private
 
-   def server_ident(vm)
-      "VM id:#{vm.id} name:'#{vm.name}'"
+    def change_resource_state(state, type, id, _data)
+      $lenovo_log.info("Change the stae of resource: #{type} instance: #{id}")
+      raise BadRequestError, "Must specify an id for starting a #{type} resource" unless id
+
+      api_action(type, id) do |klass|
+        server = resource_search(id, type, klass)
+        api_log_info(" Processing request to #{state} #{server_ident(server)}")
+        desc = "#{state}"
+        task_id = queue_object_action(server, desc, :method_name => state, :role => :ems_operations)
+        action_result(true, desc, :task_id => task_id)
+      end
+    end
+
+   def server_ident(server)
+      "Server instance: #{server.id} name:'#{server.name}'"
     end
   
   end 
