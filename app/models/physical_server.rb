15362328509e22a73036c4d1d57b79490f3b0ba9
Tercio de Melo <terciodemelo@gmail.com>
2017-02-02 15:15:03 -0300

    Adds Firmware models, controllers, and physical serers controllers

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -4,8 +4,7 @@ class PhysicalServer < ApplicationRecord
   acts_as_miq_taggable
 
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
-
-  default_value_for :enabled, true
+  has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
 
   def name_with_details
     details % {
