855650c72eabed24148f5486548e7f7ca73f8499
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2017-02-02 17:49:06 -0300

    [PhysicalServers] Modifies in models
    1. ems_refresh.rb       -> Inserts a extends to SaveInvetoryPhysicalInfra
    2. physical_server.rb   -> Exclude the default_value :enabled

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -3,7 +3,6 @@ class PhysicalServer < ApplicationRecord
   include_concern 'Operations'
 
   acts_as_miq_taggable
-
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
   has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
   has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
