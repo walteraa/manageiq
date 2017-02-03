9155c8007ca24035fc71287d380511a641fc13b4
Rodney H. Brown <rbrown4@lenovo.com>
2017-02-03 14:51:09 -0500

    Merge branch 'master' of http://github.com/ManageIQ/manageiq into development

--- app/models/physical_server.rb
+++ app/models/physical_server.rb
@@ -6,7 +6,7 @@ class PhysicalServer < ApplicationRecord
   belongs_to :ext_management_system, :foreign_key => :ems_id, :class_name => "ManageIQ::Providers::PhysicalInfraManager"
   has_many :firmwares, :foreign_key => "ph_server_uuid", :primary_key => "uuid"
   has_one :host, :foreign_key => "service_tag", :primary_key => "serialNumber"
-
+  
   def name_with_details
     details % {
       :name => name,
@@ -56,6 +56,5 @@ class PhysicalServer < ApplicationRecord
     #TODO (walteraa) remove bypass
     true
   end
-
 
 end
