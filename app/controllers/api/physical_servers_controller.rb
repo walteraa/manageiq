a573e14075864ce50a06ea1ab42c998fb3b0accf
Maysa Macedo <maysa.souza@ccc.ufcg.edu.br>
2017-02-06 17:56:42 -0300

    Fixed bug occured when physical server is nil

--- app/controllers/api/physical_servers_controller.rb
+++ app/controllers/api/physical_servers_controller.rb
@@ -1,20 +1,20 @@
 module Api
   class PhysicalServersController < BaseController
     include Subcollections::Firmwares
-  
+
     def show
       if params[:c_id]
         physical_server = PhysicalServer.find(params[:c_id])
         response_payload = physical_server.as_json
         firmwares = physical_server.firmwares.map(&:as_json)
         response_payload["firmwares"] = firmwares
-        response_payload['host'] = physical_server.host.id
+        response_payload['host'] = physical_server.host&.id
 
         render json: response_payload
       else
         super
       end
     end
-  
+
   end
 end
